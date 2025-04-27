8b93582b33f4ca705a83bf00de47ce139a6f4395
Ciaotihovisto <62794482+Ciaotihovisto@users.noreply.github.com>
2025-04-27 20:08:51 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,21 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
+self.addEventListener('install', (event) => {
+  event.waitUntil(
     caches.open('baby-tracker-cache').then((cache) => {
       return cache.addAll([
         './',
         './index.html',
-        './app.js',
         './manifest.json',
+        './app.js',
         'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css'
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
