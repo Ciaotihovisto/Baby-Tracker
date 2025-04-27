54761098632053f94aef998a1dc5e2b3c51bffe7
Ciaotihovisto <62794482+Ciaotihovisto@users.noreply.github.com>
2025-04-27 19:49:51 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('baby-tracker-cache').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './app.js',
+        './manifest.json',
+        'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
